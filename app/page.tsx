fa6c74504cb9f7153bd2fdc93b1f6349d2fa1041
roopsBee <35971830+roopsBee@users.noreply.github.com>
2023-05-30 08:48:19 +0100

    dynamic import Game

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
-import Game from '../components/Game';
+import dynamic from 'next/dynamic';
+
+const Game = dynamic(() => import('../components/Game'), { ssr: false });
 
 export default function Home() {
 	return (
